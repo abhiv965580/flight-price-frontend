2d338d28d9cd8d0e0f3a8fd2c9bbc77d9148abf3
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-08-16 01:16:23 +0530

    skeleton designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { TrendingFlat } from "@mui/icons-material";
 
 function App() {
+  const [date, setDate] = React.useState<Dayjs | null>(dayjs());
+  const [source, setSource] = React.useState<string>("");
+  const [destination, setDestination] = React.useState<string>("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Card
+          sx={{
+            minWidth: 275,
+            width: "80%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: 5,
+            margin: 2,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CardContent>
+            <Typography
+              variant="h3"
+              component="div"
+              sx={{ m: 1.5, textAlign: "center" }}
+            >
+              Flights Price Web App
+            </Typography>
+          </CardContent>
+          <CardContent sx={{ display: "flex", gap: 5 }}>
+            <TextField
+              id="input-source"
+              label="Source"
+              variant="outlined"
+              value={source}
+              onChange={(e) => setSource(e.target.value)}
+            />
+            <TrendingFlat sx={{ fontSize: 50 }} />
+            <TextField
+              id="input-destination"
+              label="Destination"
+              variant="outlined"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+            />
+          </CardContent>
+          <CardContent>
+            <DatePicker
+              label="Date"
+              format="DD/MM/YYYY"
+              value={date}
+              onChange={(newValue) => setDate(newValue)}
+              disablePast
+            />
+          </CardContent>
+          <CardActions>
+            <Button size="large" variant="contained">
+              Get Prices
+            </Button>
+          </CardActions>
+        </Card>
+      </LocalizationProvider>
     </div>
   );
 }
